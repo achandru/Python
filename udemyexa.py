10859e4649caa258664fcf73fdd2ac3869963a86
CHANDRASEKARAN A <achandrumca@gmail.com>
2017-03-11 20:31:35 +0530

    Run this Program

---
+++ udemyexa.py
@@ -0,0 +1,13 @@
+"""emails = ['me@gmail.com','you@yahoo.com','yes@gmail.com']
+for item in emails:
+if 'gmail' in item:
+	print(item)"""
+"""for i in (1,2,3):
+	print(i+1)"""
+"""a="Tricky"
+for i in a[:3]:
+    print(i)"""
+lst=["Terribly Tricky"]
+for word in lst:
+	for letter in word[-6:]:
+		print(letter)
